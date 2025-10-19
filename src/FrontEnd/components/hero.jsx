fe618866c2b6e2a40809e0506e61935252f9336b
William Ramsay <william_ramsay@sfu.ca>
2025-10-18 17:16:42 -0700

    Added some styles

--- src/FrontEnd/components/hero.jsx
+++ src/FrontEnd/components/hero.jsx
@@ -4,17 +4,14 @@ import { Button } from "@/components/ui/button"
 export default function Hero() {
   return (
     
-    <div className="bg-[./Assignments.jpg] min-h-screen flex items-center justify-center "  id="Hero">
+    <div className="bg-[url('./public/Assignments.jpg')] bg-cover bg-center min-h-screen flex items-center justify-center "  id="Hero">
       <div className="text-center animate-fade-in text-white px-2 max-w-2xl">
-        <h1 className="text-6xl font-bold mb-6 "> 
-          Assignment Checker
+        <h1 className="font-[PermanentMarker-Regular] fa-map-marker-alt translate-x-14 text-6xl text-red-400 font-bold mb-6 "> 
+              Assignment Checker
         </h1>
-        <p className="text-xl text-slate-300 mb-8">
-          Search for your assignments!
+        <p className="font-[PermanentMarker-Regular] text-xl text-blue-400 mb-8">
+             Search for your assignments!
         </p>
-        <Button size="lg" variant="default">
-            <a href="#find">Find Assignments</a>
-        </Button>
         
       </div>
     </div>
